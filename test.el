7c41138598aa1508dae4747cdc96a8ff4f507983
Kevin Ziegler <ziegler.kevin@heb.com>
2021-07-16 12:49:22 -0500

    Add the beginnings of tests
    Pretty sure I never had these working, but I'll keep them for
    posterity/reference until I can come back to this work.

---
+++ test.el
@@ -0,0 +1,21 @@
+(require 'ert)
+
+(ert-deftest lsp-java-lombok/test-jar-file-with-nil-version ()
+  "Filename has no version information when version is nil."
+  (setq-local lsp-java-lombok/version nil)
+  (should (equal  "lombok.jar" (lsp-java-lombok/jar-file))))
+
+(ert-deftest lsp-java-lombok/test-jar-file-with-version ()
+  "Filename includes version when version is set."
+  (setq-local lsp-java-lombok/version "1.0")
+  (should (equal  "lombok-1.0.jar" (lsp-java-lombok/jar-file))))
+
+(ert-deftest lsp-java-lombok/test-download-jar ()
+  "Downloads correct Lombok jar from configured url to configured path."
+  (setq-local lsp-java-lombok/jar-url-base "https://test.com")
+  (setq-local lsp-java-lombok/dir "/test/path")
+  (setq-local lsp-java-lombok/version "1.0")
+  (cl-flet ((url-copy-file (src dest)
+                           (should (equal src "https://test.com/lombok-1.0.jar"))
+                           (should (equal dest "/test/path/lombok-1.0.jar")))
+            (lsp-java-lombok/download-jar))))
